b49a635997461f5e951342dd8bd85f80b8b5e7d0
John Wason <wason@wasontech.com>
2021-02-26 23:26:53 -0500

    Vue working with examples on welcome panel

--- src/pyri/webui_browser/panels/welcome_panel.py
+++ src/pyri/webui_browser/panels/welcome_panel.py
@@ -4,13 +4,26 @@ from .. import PyriWebUIBrowser
 import importlib_resources
 import js
 
+from ..util import jsbind
 
 class PyriWelcomePanel(PyriWebUIBrowserPanelBase):
 
-    def __init__(self, welcome_counter):
-        self.welcome_counter = welcome_counter
+    def __init__(self):
+        pass
 
-        self.welcome_counter["$data"].count=20
+    def init_vue(self, vue):
+        self.vue =vue
+        self.vue["$data"].count=20
+
+    @jsbind
+    def increment(self, js_this, evt):
+        print(int(evt.target.getAttribute("data-joint")))
+        self.vue["$data"].count+=1
+
+    @jsbind
+    def decrement(self, js_this, evt):
+        print(int(evt.target.getAttribute("data-joint")))
+        self.vue["$data"].count-=1
 
 async def add_welcome_panel(panel_type: str, core: PyriWebUIBrowser, parent_element: Any):
     assert panel_type == "welcome"
@@ -28,7 +41,6 @@ async def add_welcome_panel(panel_type: str, core: PyriWebUIBrowser, parent_elem
     gl = core.layout.layout
 
     def register_welcome(container, state):
-        print(welcome_panel_html)
         container.getElement().html(welcome_panel_html)
 
     core.layout.register_component("welcome",register_welcome)
@@ -37,15 +49,23 @@ async def add_welcome_panel(panel_type: str, core: PyriWebUIBrowser, parent_elem
     
     core.layout.add_panel_menu_item("welcome", "Welcome")
 
+    welcome_panel_obj = PyriWelcomePanel()
+
     welcome_counter = js.Vue.new({
         "el": "#welcome_counter",
         "data": {
             "count": 10 
+        },
+        "methods":
+        {
+            "increment": welcome_panel_obj.increment,
+            "decrement": welcome_panel_obj.decrement
         }
     })
 
-    print(dir(welcome_counter))
-
-    welcome_panel_obj = PyriWelcomePanel(welcome_counter)
+    welcome_panel_obj.init_vue(welcome_counter)
+   
+    js.window.welcome_counter = welcome_counter
+    js.window.welcome_panel_obj = welcome_panel_obj
 
     return welcome_panel_obj
