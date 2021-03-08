236d6668adc4b14332c8e3397e45fccd8b6ce10e
John Wason <wason@wasontech.com>
2021-03-08 18:35:06 -0500

    Begin improving jog

--- src/pyri/webui_browser/panels/jog_panel.py
+++ src/pyri/webui_browser/panels/jog_panel.py
@@ -10,6 +10,7 @@ import numpy as np
 class PyriJogPanel(PyriWebUIBrowserPanelBase):
     def __init__(self):
         self.vue = None
+        self.mousedown = False
 
     def init_vue(self,vue):
         self.vue = vue
@@ -103,17 +104,63 @@ class PyriJogPanel(PyriWebUIBrowserPanelBase):
             ret.append(v)        
         return ret
 
+    def current_robot_mode(self, vue, *args):
+        current_robot = vue["$data"].current_robot
+        if current_robot is None:
+            return "Invalid"
+
+        mode = "Invalid"
+
+        try:
+            e_state = vue["$store"].state.devices_states.devices_states[current_robot].state
+            if e_state is not None:
+                for e in e_state:
+                    if e.type == "com.robotraconteur.robotics.robot.RobotState":
+                        command_mode = e.state_data.command_mode
+
+                        if command_mode < 0:
+                            mode = "Error"
+                        elif command_mode == 0:
+                            mode = "Halt"
+                        elif command_mode == 1:
+                            mode = "Jog"
+                        elif command_mode == 2:
+                            mode = "Trajectory"
+                        elif command_mode == 3:
+                            mode = "Position Command"
+                        elif command_mode == 4:
+                            mode = "Velocity Command"
+                        elif command_mode == 5:
+                            mode = "Homing"
+                        else:
+                            mode = f"Unknown ({command_mode})"
+        except AttributeError:
+            traceback.print_exc()
+        except KeyError:
+            traceback.print_exc()
+
+        return mode
+
     def jog_decrement_mousedown(self, joint_index):
         print(f"jog_decrement_mousedown: {joint_index}")
-
-    def jog_decrement_mouseup(self, joint_index):
-        print(f"jog_decrement_mouseup: {joint_index}")
 
     def jog_increment_mousedown(self, joint_index):
         print(f"jog_increment_mousedown: {joint_index}")
 
-    def jog_increment_mouseup(self, joint_index):
-        print(f"jog_increment_mouseup: {joint_index}")
+    def set_jog_mode(self, evt):
+        js.alert("Enable jog")
+
+    def set_halt_mode(self, evt):
+        js.alert("Enable jog")
+
+    def mousedown_evt(self,evt):
+        self.mousedown = True
+
+    def mouseup_evt(self,evt):
+        self.mousedown = False
+
+    def mouseleave_evt(self,evt):
+        self.mousedown = False
 
 
 async def add_jog_panel(panel_type: str, core: PyriWebUIBrowser, parent_element: Any):
@@ -154,14 +201,18 @@ async def add_jog_panel(panel_type: str, core: PyriWebUIBrowser, parent_element:
         "methods":
         {
             "jog_decrement_mousedown": jog_panel_obj.jog_decrement_mousedown,
-            "jog_decrement_mouseup": jog_panel_obj.jog_decrement_mouseup,
             "jog_increment_mousedown": jog_panel_obj.jog_increment_mousedown,
-            "jog_increment_mouseup": jog_panel_obj.jog_increment_mouseup
+            "set_jog_mode": jog_panel_obj.set_jog_mode,
+            "set_halt_mode": jog_panel_obj.set_halt_mode,
+            "mousedown": jog_panel_obj.mousedown_evt,
+            "mouseup": jog_panel_obj.mouseup_evt,
+            "mouseleave": jog_panel_obj.mouseleave_evt
         },
         "computed": 
         {
             "current_robot_options": jog_panel_obj.current_robot_options,
-            "joint_state": jog_panel_obj.joint_state
+            "joint_state": jog_panel_obj.joint_state,
+            "current_robot_mode": jog_panel_obj.current_robot_mode
         },
         "watch":
         {
