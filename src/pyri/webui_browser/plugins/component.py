7ef4719b1dc0d5b4aee02b5f76c9562d695a7513
John Wason <wason@wasontech.com>
2022-09-19 00:19:58 -0400

    Fix return decorator in component.py

--- src/pyri/webui_browser/plugins/component.py
+++ src/pyri/webui_browser/plugins/component.py
@@ -14,7 +14,7 @@ class PyriWebUIBrowserComponentPluginFactory:
 def get_webui_browser_component_factories() -> List[PyriWebUIBrowserComponentPluginFactory]:
     return plugin_util.get_plugin_factories("pyri.plugins.webui_browser_component")
 
-def register_all_webui_browser_components() -> Dict[str,Any]:
+def register_all_webui_browser_components() -> None:
     factories = get_webui_browser_component_factories()
     for factory in factories:
         factory.register_components()
